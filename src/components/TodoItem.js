c255887a1bc250d0ece96087a36708fb9941f0e2
sabrine loussaief <loussaiefsabrine@gmail.comgit config --global user.email loussaiefsabrine@gmail.com>
2022-03-07 12:15:06 +0100

    my checkpoint redux

---
+++ src/components/TodoItem.js
@@ -0,0 +1,62 @@
+import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { AiFillEdit } from "react-icons/ai";
+import { IoCheckmarkDoneSharp, IoClose } from "react-icons/io5";
+
+const TodoItem = (props) => {
+  const { item, updateTodo, removeTodo, completeTodo } = props;
+
+  const inputRef = useRef(true);
+
+  const changeFocus = () => {
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  };
+
+  const update = (id, value, e) => {
+    if (e.which === 13) {
+      
+      updateTodo({ id, item: value });
+      inputRef.current.disabled = true;
+    }
+  };
+  return (
+    <motion.li
+      key={item.id}
+      className="card"
+    >
+      <textarea
+        ref={inputRef}
+        disabled={inputRef}
+        defaultValue={item.item}
+        onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
+      />
+      <div className="btns">
+        <motion.button
+          onClick={() => changeFocus()}
+        >
+          {" "}
+          <AiFillEdit />{" "}
+        </motion.button>
+        {item.completed === false && (
+          <motion.button
+            style={{ color: "green" }}
+            onClick={() => completeTodo(item.id)}
+          >
+            <IoCheckmarkDoneSharp />
+          </motion.button>
+        )}
+        <motion.button
+          style={{ color: "red" }}
+          onClick={() => removeTodo(item.id)}
+        >
+          {" "}
+          <IoClose />
+        </motion.button>{" "}
+      </div>
+      {item.completed && <span className="completed">done</span>}
+    </motion.li>
+  );
+};
+
+export default TodoItem;
